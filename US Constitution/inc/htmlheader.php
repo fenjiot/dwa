396ab2d2dbff80d36b3bcbd8a2c156ad35375efe
fenjiot <user@fen>
2012-09-21 20:59:55 -0400

    p1 push

--- US Constitution/inc/htmlheader.php
+++
@@ -1,5 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/constitution.css" type="text/css" />
-<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection" />
-<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print" />
-<!--[if lt IE 8]><link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection" /><![endif]-->
--- p1/inc/header.php
+++
@@ -1,3 +0,0 @@
-<div id="header" class="span-24 last">
-	<h1>Project 1</h1>
-</div>
