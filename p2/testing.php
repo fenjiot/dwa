8d6a70ee3c5c0f60beab47a96501a95fd81d302e
fenjiot <user@fen>
2012-09-30 22:20:20 -0400

    testing boxes...
    and pushed dwa index.html

--- p2/testing.php
+++ p2/testing.php
@@ -4,19 +4,19 @@
 </head>
 	
 	<?
-	$day=date("G");
-
-	$country = geoip_country_code_by_name('locahost:8888');
-	if ($country) {
-	    echo 'This host is located in: ' . $country;
-	}
-
-	?>
+	$boxes = "";
 	
+	for($i = 1; $i <= 10; $i++) {
+		$var1 = rand(0,500);
+		$var2 = rand(0,500);
+		$boxes .= "<div style='width:{$var1}px; height:{$var2}px; float:left; margin:4px; background-color:red'></div>";
+	};
+	
+	?>	
+		
 <body>
 
-	Today is <?=$day?>.
-	
+	<?=$boxes?>
 
 </body>
 </html>
