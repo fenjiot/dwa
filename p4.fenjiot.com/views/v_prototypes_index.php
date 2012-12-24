a2fd564f6d4e3bc9cd77a0277bdcdfdb70550239
fenjiot <user@fen>
2012-12-24 04:01:59 -0500

    p4 - minor prototypes page

--- p4.fenjiot.com/views/v_prototypes_index.php
+++ p4.fenjiot.com/views/v_prototypes_index.php
@@ -27,7 +27,7 @@
 		<div id="main_information">
 			<? if($products == array ()): ?>
 			 	<h2>There are no products in the system.&nbsp Nothing to see here.</h2>
-			 	<a class="mediumwords" href="/manage/addproduct/"> &nbsp&gt;&gt;Add a product</a>
+			 	<a class="mediumwords" href="/manage/addproduct/"> &nbsp&gt;&gt; Sign up and Add a product</a>
 			<? else: ?>			
 				<? foreach($products as $key => $product): ?>
 					<div class="products tiled">
